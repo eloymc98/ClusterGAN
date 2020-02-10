3d088dcc1452b9c6774937261ae5f4fd8dd18e97
Eloy Marin <eloymc16@gmail.com>
2020-02-10 14:01:32 +0100

    test

--- Image_ClusterGAN.py
+++ Image_ClusterGAN.py
@@ -240,12 +240,10 @@ class clusGAN(object):
             xtrue = data_recon[pt_indx, :]
 
             zhats_gen, zhats_label = self.sess.run([self.z_infer_gen, self.z_infer_label], feed_dict={self.x: xtrue})
-            # zhats_label
-            # labelsss[pt_indx, :] = np.where(zhats_label == 1)
+
+            labelsss[pt_indx, :] = np.argmax(zhats_label, axis=1)
             if b == 1:
-                print(f'zhats_label {zhats_label}')
-                #print(f'zhats_labelvsvsdvds {np.where(zhats_label == 1)}')
-                # print(f'labels {labelsss}')
+                print(f'labels {labelsss}')
             latent[pt_indx, :] = np.concatenate((zhats_gen, zhats_label), axis=1)
 
         if self.beta_cycle_gen == 0:
