dfcb8647503bcae108d14c91d645b6109944fef6
Eloy Marin <eloymc16@gmail.com>
2020-02-17 16:45:48 +0100

    test

--- Image_ClusterGAN.py
+++ Image_ClusterGAN.py
@@ -23,6 +23,7 @@ tf.set_random_seed(0)
 
 def copytree(src, dst, symlinks=False, ignore=None):
     for item in os.listdir(src):
+        print(item)
         s = os.path.join(src, item)
         d = os.path.join(dst, item)
         if os.path.isdir(s):
