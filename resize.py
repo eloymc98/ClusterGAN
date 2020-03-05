eb847dd9649ec561047ece887d65566f52f94ad0
Eloy Marin <eloymc16@gmail.com>
2020-03-05 11:30:57 +0100

    test

--- resize.py
+++ resize.py
@@ -6,11 +6,14 @@ import cv2
 def resize_google_colors(size):
     path = "/content/ClusterGAN/colors/google_colors"
     dirs = os.listdir(path)
+    print(dirs)
     for item in dirs:
         subdir = path + '/' + item
+        print(subdir)
         if os.path.isdir(subdir):
             for image in os.listdir(subdir):
                 if os.path.isfile(image):
+                    print(image)
                     bgr = cv2.imread(image)
                     # img = cv2.cvtColor(bgr, cv2.COLOR_BGR2LAB)
                     img = cv2.resize(bgr, (size, size), interpolation=cv2.INTER_AREA)
