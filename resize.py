006800a5e7536f3ebe65a719c545703d8d9895a2
Eloy Marin <eloymc16@gmail.com>
2020-03-05 18:47:57 +0100

    test

--- resize.py
+++ resize.py
@@ -13,7 +13,6 @@ def resize_google_colors(size):
         if os.path.isdir(subdir):
             for image in os.listdir(subdir):
                 if os.path.isfile(subdir + '/' + image):
-                    print(image)
                     bgr = cv2.imread(subdir + '/' + image)
                     # img = cv2.cvtColor(bgr, cv2.COLOR_BGR2LAB)
                     img = cv2.resize(bgr, (size, size), interpolation=cv2.INTER_AREA)
@@ -23,10 +22,8 @@ def resize_google_colors(size):
 def resize_termisk(size):
     path = "/content/ClusterGAN/termisk/termisk_dataset"
     dirs = os.listdir(path)
-    print(dirs)
     for item in dirs:
         subdir = path + '/' + item
-        print(subdir)
         if os.path.isdir(subdir):
             dirs2 = os.listdir(subdir)
             for item2 in dirs2:
@@ -34,7 +31,6 @@ def resize_termisk(size):
                 if os.path.isdir(subdir2):
                     for image in os.listdir(subdir2):
                         if os.path.isfile(subdir2 + '/' + image):
-                            print(image)
                             grey = cv2.imread(subdir2 + '/' + image, cv2.IMREAD_GRAYSCALE)
                             img = cv2.resize(grey, (size, size), interpolation=cv2.INTER_AREA)
                             cv2.imwrite(subdir2 + '/' + image, img)
