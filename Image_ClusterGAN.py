e01007c02326390d73a60c76e6d1a1cb81435503
Eloy Marin <eloymc16@gmail.com>
2020-03-18 15:56:51 +0100

    test

--- Image_ClusterGAN.py
+++ Image_ClusterGAN.py
@@ -277,6 +277,8 @@ class clusGAN(object):
                 batch_size = 1008
             elif self.num_classes == 5:
                 batch_size = 1000
+            elif self.num_classes == 11:
+                batch_size = 1001
             else:
                 batch_size = 1000
             label_index = np.tile(np.arange(self.num_classes), int(np.ceil(batch_size * 1.0 / self.num_classes)))
