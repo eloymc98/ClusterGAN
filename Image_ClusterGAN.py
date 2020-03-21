b9878329c2b39fb59cf04292d171629feb82f894
Eloy Marin <eloymc16@gmail.com>
2020-03-21 18:10:22 +0100

    test

--- Image_ClusterGAN.py
+++ Image_ClusterGAN.py
@@ -242,7 +242,7 @@ class clusGAN(object):
             checkpoint_dir = 'pre_trained_models/{}/{}_{}_z{}_cyc{}_gen{}'.format(self.data, self.model, self.sampler,
                                                                                   self.z_dim, self.beta_cycle_label,
                                                                                   self.beta_cycle_gen)
-            if self.data in ('termisk', 'cifar', 'mnist', 'fashion', 'colors', 'colors_new'):
+            if self.data in ('termisk', 'cifar', 'mnist', 'fashion', 'colors', 'colors_new', 'synthetic_colors'):
                 checkpoint_dir = 'pre_trained_models/{}'.format(self.data)
         else:
             if timestamp == '':
