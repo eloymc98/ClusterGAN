eaad17cf5dde620285eaf22ca0cdf3b732e4fa2d
Eloy Marin <eloymc16@gmail.com>
2020-03-25 09:03:14 +0100

    test

--- termisk/clus_wgan.py
+++ termisk/clus_wgan.py
@@ -9,7 +9,7 @@ def leaky_relu(x, alpha=0.2):
 
 class Discriminator(object):
     # x_dim = 28*28
-    def __init__(self, x_dim=784):
+    def __init__(self, x_dim=4096):
         self.x_dim = x_dim
         self.name = 'termisk/clus_wgan/d_net'
 
@@ -24,33 +24,43 @@ class Discriminator(object):
             # Reshape to 3d
             print(f'Discriminator x before: {x.shape}')
 
-            x = tf.reshape(x, [bs, 28, 28, 1])
+            x = tf.reshape(x, [bs, 64, 64, 1])
             print(f'Discriminator x after: {x.shape}')
-
-            # output = conv2d(input, num_outputs, kernel_size, stride)
-            # tf.indetity -> Return a tensor with the same shape and contents as input
             conv1 = tc.layers.convolution2d(
-                x, 64, [4, 4], [2, 2],
+                x, 32, [4, 4], [2, 2],
                 weights_initializer=tf.random_normal_initializer(stddev=0.02),
                 activation_fn=tf.identity
             )
             conv1 = leaky_relu(conv1)
-            print(f'Discriminator conv1: {conv1.shape}')
+            # out = (bs,32,32,32)
 
+            # output = conv2d(input, num_outputs, kernel_size, stride)
+            # tf.indetity -> Return a tensor with the same shape and contents as input
             conv2 = tc.layers.convolution2d(
-                conv1, 128, [4, 4], [2, 2],
+                conv1, 64, [4, 4], [2, 2],
                 weights_initializer=tf.random_normal_initializer(stddev=0.02),
                 activation_fn=tf.identity
             )
             conv2 = leaky_relu(conv2)
-            print(f'Discriminator conv2 before: {conv2.shape}')
-            conv2 = tcl.flatten(conv2)
-            print(f'Discriminator conv2 after: {conv2.shape}')
+            # out = (bs,16,16,64)
+            print(f'Discriminator conv2: {conv2.shape}')
+
+            conv3 = tc.layers.convolution2d(
+                conv2, 128, [4, 4], [2, 2],
+                weights_initializer=tf.random_normal_initializer(stddev=0.02),
+                activation_fn=tf.identity
+            )
+            conv3 = leaky_relu(conv3)
+            # out = (bs,8,8,128)
+            print(f'Discriminator conv3 before: {conv3.shape}')
+            conv3 = tcl.flatten(conv3)
+            # out = (bs,8192)
+            print(f'Discriminator conv3 after: {conv3.shape}')
             # Flatten conv2 output to use it as input for fc.
 
             # fc = fully_connected(input, num_outputs)
             fc1 = tc.layers.fully_connected(
-                conv2, 1024,
+                conv3, 2048,
                 weights_initializer=tf.random_normal_initializer(stddev=0.02),
                 activation_fn=tf.identity
             )
@@ -66,7 +76,7 @@ class Discriminator(object):
 
 
 class Generator(object):
-    def __init__(self, z_dim=10, x_dim=784):
+    def __init__(self, z_dim=10, x_dim=4096):
         self.z_dim = z_dim
         self.x_dim = x_dim
         self.name = 'termisk/clus_wgan/g_net'
@@ -78,7 +88,7 @@ class Generator(object):
             print(f'Generator z: {z.shape}')
 
             fc1 = tc.layers.fully_connected(
-                z, 1024,
+                z, 2048,
                 weights_initializer=tf.random_normal_initializer(stddev=0.02),
                 weights_regularizer=tc.layers.l2_regularizer(2.5e-5),
                 activation_fn=tf.identity
@@ -87,17 +97,17 @@ class Generator(object):
             fc1 = tf.nn.relu(fc1)
             print(f'Generator fc1: {fc1.shape}')
             fc2 = tc.layers.fully_connected(
-                fc1, 7 * 7 * 128,
+                fc1, 8 * 8 * 128,
                 weights_initializer=tf.random_normal_initializer(stddev=0.02),
                 weights_regularizer=tc.layers.l2_regularizer(2.5e-5),
                 activation_fn=tf.identity
             )
             print(f'Generator fc2 before: {fc2.shape}')
-            fc2 = tf.reshape(fc2, tf.stack([bs, 7, 7, 128]))
+            fc2 = tf.reshape(fc2, tf.stack([bs, 8, 8, 128]))
             fc2 = tc.layers.batch_norm(fc2)
             fc2 = tf.nn.relu(fc2)
             print(f'Generator fc2 after: {fc2.shape}')
-
+            # out = (bs,8,8,128)
             conv1 = tc.layers.convolution2d_transpose(
                 fc2, 64, [4, 4], [2, 2],
                 weights_initializer=tf.random_normal_initializer(stddev=0.02),
@@ -107,20 +117,29 @@ class Generator(object):
             conv1 = tc.layers.batch_norm(conv1)
             conv1 = tf.nn.relu(conv1)
             print(f'Generator conv1: {conv1.shape}')
-
+            # out = (bs,16,16,64)
             conv2 = tc.layers.convolution2d_transpose(
-                conv1, 1, [4, 4], [2, 2],
+                conv1, 32, [4, 4], [2, 2],
                 weights_initializer=tf.random_normal_initializer(stddev=0.02),
                 weights_regularizer=tc.layers.l2_regularizer(2.5e-5),
                 activation_fn=tf.sigmoid
             )
-            print(f'Generator conv2 before: {conv2.shape}')
-            # shape: (bs, 28, 28, 1)
-            # Para entrenar con imagenes más grandes, añadir más conv!!!
-
-            conv2 = tf.reshape(conv2, tf.stack([bs, self.x_dim]))
+            conv2 = tc.layers.batch_norm(conv2)
+            conv2 = tf.nn.relu(conv2)
+            print(f'Generator conv2: {conv2.shape}')
+            # out = (bs,32,32,32)
+            conv3 = tc.layers.convolution2d_transpose(
+                conv2, 1, [4, 4], [2, 2],
+                weights_initializer=tf.random_normal_initializer(stddev=0.02),
+                weights_regularizer=tc.layers.l2_regularizer(2.5e-5),
+                activation_fn=tf.sigmoid
+            )
+            conv3 = tc.layers.batch_norm(conv3)
+            conv3 = tf.nn.relu(conv3)
+            # out = (bs,64,64,1)
+            conv3 = tf.reshape(conv3, tf.stack([bs, self.x_dim]))
             print(f'Generator conv2 after: {conv2.shape}')
-            return conv2
+            return conv3
 
     @property
     def vars(self):
@@ -128,7 +147,7 @@ class Generator(object):
 
 
 class Encoder(object):
-    def __init__(self, z_dim=10, dim_gen=10, x_dim=784):
+    def __init__(self, z_dim=10, dim_gen=10, x_dim=4096):
         self.z_dim = z_dim
         self.dim_gen = dim_gen
         self.x_dim = x_dim
@@ -140,28 +159,38 @@ class Encoder(object):
                 vs.reuse_variables()
             bs = tf.shape(x)[0]
             print(f'Encoder x before: {x.shape}')
-            x = tf.reshape(x, [bs, 28, 28, 1])
+            x = tf.reshape(x, [bs, 64, 64, 1])
             print(f'Encoder x after: {x.shape}')
             conv1 = tc.layers.convolution2d(
-                x, 64, [4, 4], [2, 2],
+                x, 32, [4, 4], [2, 2],
                 weights_initializer=tf.random_normal_initializer(stddev=0.02),
-                weights_regularizer=tc.layers.l2_regularizer(2.5e-5),
                 activation_fn=tf.identity
             )
             conv1 = leaky_relu(conv1)
-            print(f'Encoder conv1: {conv1.shape}')
+            # out = (bs,32,32,32)
             conv2 = tc.layers.convolution2d(
-                conv1, 128, [4, 4], [2, 2],
+                conv1, 64, [4, 4], [2, 2],
                 weights_initializer=tf.random_normal_initializer(stddev=0.02),
                 weights_regularizer=tc.layers.l2_regularizer(2.5e-5),
                 activation_fn=tf.identity
             )
             conv2 = leaky_relu(conv2)
-            print(f'Encoder conv2 before: {conv2.shape}')
-            conv2 = tcl.flatten(conv2)
-            print(f'Encoder conv2 after: {conv2.shape}')
+            print(f'Encoder conv2: {conv2.shape}')
+            # out = (bs,16,16,64)
+            conv3 = tc.layers.convolution2d(
+                conv2, 128, [4, 4], [2, 2],
+                weights_initializer=tf.random_normal_initializer(stddev=0.02),
+                weights_regularizer=tc.layers.l2_regularizer(2.5e-5),
+                activation_fn=tf.identity
+            )
+            conv3 = leaky_relu(conv3)
+            # out = (bs,8,8,128)
+            print(f'Encoder conv3 before: {conv3.shape}')
+            conv3 = tcl.flatten(conv3)
+            print(f'Encoder conv3 after: {conv3.shape}')
+            # out = (bs,8192)
             fc1 = tc.layers.fully_connected(
-                conv2, 1024,
+                conv3, 2048,
                 weights_initializer=tf.random_normal_initializer(stddev=0.02),
                 weights_regularizer=tc.layers.l2_regularizer(2.5e-5),
                 activation_fn=tf.identity
