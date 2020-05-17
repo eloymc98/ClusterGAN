11871f1034ef502b965c9c22266bb9e479acb4eb
Eloy Marin <eloymc16@gmail.com>
2020-05-17 19:18:43 +0200

    test

--- Image_ClusterGAN.py
+++ Image_ClusterGAN.py
@@ -521,7 +521,7 @@ class clusGAN(object):
 
         co_mat = pd.crosstab(df.label, df.cluster)
         sn.set(font_scale=1.4)
-        plt.figure(figsize=(20, 20))
+        plt.figure(figsize=(8, 6))
         sn.heatmap(co_mat)
         plt.savefig(f'{self.data}_{self.num_classes}_matrix.png')
         # tengo labels reales y label generadas y el mapeo correspondiente
