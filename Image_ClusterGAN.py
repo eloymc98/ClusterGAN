ded126b22f36c68ce90650b66e84d2bc5555391b
Eloy Marin <eloymc16@gmail.com>
2020-05-26 15:22:01 +0200

    test

--- Image_ClusterGAN.py
+++ Image_ClusterGAN.py
@@ -728,7 +728,7 @@ class clusGAN(object):
         df['cluster'] = labels_predicted.astype(np.uint8)
         print(df.head())
 
-        co_mat = pd.crosstab(df.label, df.cluster, normalize='all')
+        co_mat = pd.crosstab(df.label, df.cluster, normalize='index')
         sn.set(font_scale=1.4)
         sn.heatmap(co_mat)
         plt.savefig('co-ocurrence.png')
